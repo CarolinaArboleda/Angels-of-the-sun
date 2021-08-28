1fd6325ab8b9ac2379a32d8b27428c8ba2cc9b0e
CarolinaArboleda <carolina.arboledaa@upb.edu.co>
2021-08-27 20:45:45 -0500

    cámara y colores

---
+++ Angels of the sun/Assets/Scripts/Camara.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    public Transform emma;
+    public Vector3 desplazamiento;
+  
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(emma.position.x + desplazamiento.x, emma.position.y + desplazamiento.y, desplazamiento.z);
+    }
+}
