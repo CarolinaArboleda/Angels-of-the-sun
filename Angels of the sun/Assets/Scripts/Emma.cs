ca9b58b494eebd07ff477179294c69887bb6f2d4
CarolinaArboleda <carolina.arboledaa@upb.edu.co>
2021-09-08 12:49:15 -0500

    salto_y_animacion

--- Angels of the sun/Assets/Scripts/Emma.cs
+++ Angels of the sun/Assets/Scripts/Emma.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class Emma : MonoBehaviour
 {
+    public float altura_salto;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(0, altura_salto);
+        }
         float horizontal = Input.GetAxis("Horizontal");
-        float jump = Input.GetAxis("Jump");
+        //float jump = Input.GetAxis("Jump");
         Vector2 position = transform.position;
         position.x = position.x + 4.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 6.0f * jump * Time.deltaTime;
+        //position.y = position.y + 6.0f * jump * Time.deltaTime;
         transform.position = position;
 
     }
