938bea35ce0fa12958e06f89ce350dbed603f3e8
CarolinaArboleda <carolina.arboledaa@upb.edu.co>
2021-09-09 20:52:33 -0500

    arreglo salto

--- Angels of the sun/Assets/Scripts/Emma.cs
+++ Angels of the sun/Assets/Scripts/Emma.cs
@@ -7,18 +7,29 @@ public class Emma : MonoBehaviour
     public float altura_salto;
     public float velocidad_movimiento;
     private Rigidbody2D rb;
+    private bool touch_floor;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        if (c.gameObject.tag.Equals("Floor"))
+        {
+            touch_floor = true;
+        }
+        else touch_floor = false;
+
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && touch_floor)
         {
             rb.velocity = new Vector2(rb.velocity.x, altura_salto);
+            touch_floor = false;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
