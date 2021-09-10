1f5ac304523f0a13c86e72be19083538ef48711c
CarolinaArboleda <carolina.arboledaa@upb.edu.co>
2021-09-10 12:05:20 -0500

    controles animación

--- Angels of the sun/Assets/Scripts/Emma.cs
+++ Angels of the sun/Assets/Scripts/Emma.cs
@@ -7,11 +7,14 @@ public class Emma : MonoBehaviour
     public float altura_salto;
     public float velocidad_movimiento;
     private Rigidbody2D rb;
+    private Animator anim;
     private bool touch_floor;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        anim.SetInteger("Estado", 0);
     }
 
     void OnCollisionEnter2D(Collision2D c)
@@ -26,6 +29,10 @@ public class Emma : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(touch_floor)
+        {
+            anim.SetInteger("Estado", 0);
+        }
         if (Input.GetKey(KeyCode.Space) && touch_floor)
         {
             rb.velocity = new Vector2(rb.velocity.x, altura_salto);
@@ -35,11 +42,13 @@ public class Emma : MonoBehaviour
         {
             rb.velocity = new Vector2(velocidad_movimiento, rb.velocity.y);
             rb.transform.localScale = new Vector2(1, 1);
+            anim.SetInteger("Estado", 1);
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb.velocity = new Vector2(-velocidad_movimiento, rb.velocity.y);
             rb.transform.localScale = new Vector2(-1, 1);
+            anim.SetInteger("Estado", 1);
         }
     }
 }
