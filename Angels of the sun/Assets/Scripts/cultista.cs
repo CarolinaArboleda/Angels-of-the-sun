1f54fd942cf466f3874afb495a408e49d3b1c5f3
CarolinaArboleda <carolina.arboledaa@upb.edu.co>
2021-10-16 18:59:55 -0500

    caminata cultista

---
+++ Angels of the sun/Assets/Scripts/cultista.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cultista : MonoBehaviour
+{
+
+    public float speed = 2.5f;
+    public float changeTime = 3.0f;
+
+    Rigidbody2D rigidbody2D;
+    float timer;
+    int direction = 1;
+
+    Transform myTransform;
+
+    void Awake()
+    {
+        myTransform = transform;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (direction == -1)
+        {
+            rigidbody2D.transform.localScale = new Vector2(-1, 1);
+        }
+
+        if (direction == 1)
+        {
+            rigidbody2D.transform.localScale = new Vector2(1, 1);
+        }
+
+        Vector2 position = rigidbody2D.position;
+
+        position.x = position.x + Time.deltaTime * speed * direction; ;
+
+        rigidbody2D.MovePosition(position);
+    }
+}
